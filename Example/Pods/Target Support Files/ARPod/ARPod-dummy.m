5e13945f0e3610be7bc11334ae95c8e1ea961c93
Mario Vargas <mariomad18@gmail.com>
2023-11-01 21:20:54 -0500

    Initial commit

---
+++ Example/Pods/Target Support Files/ARPod/ARPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ARPod : NSObject
+@end
+@implementation PodsDummy_ARPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-ARPod_Tests/Pods-ARPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ARPod_Tests
+@end
